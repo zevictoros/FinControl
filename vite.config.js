73615d2647137a22f89185686f4f1d1f91dc29c0
zevictoros <zevictoros1808@gmail.com>
2026-04-01 19:36:25 -0300

    fix: otimização de build para vercel 2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // ou @vitejs/react-swc se estiver usando
+import react from "@vitejs/react-swc"; 
 import path from "path";
 
 export default defineConfig({
